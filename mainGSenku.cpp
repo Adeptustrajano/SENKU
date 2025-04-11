0f1f2bfc76b711f029e5e3754e1013fd9e535d94
Primaris <146866638+Adeptustrajano@users.noreply.github.com>
2025-04-11 13:08:58 +0200

    mainGSenku.cpp

--- mainGSenku.cpp
+++ mainGSenku.cpp
@@ -4,6 +4,20 @@
 
 using namespace std;
 
+// main del programa que necesita [4] argumentos para la ejecución:
+// 1. Fichero de tablero
+// 2. Fichero de movimientos válidos    
+// 3. Retardo en milisegundos
+// 4. Nombre fichero de salida opcional o resultado.txt automatico 
+
+// Ejecutable en ubuntu: ./mainGSenku tablero.txt movimientos.txt 100 salida.txt o directamente con el makefile
+// argv[0] = "./mainGSenku"
+// argv[1] = "tablero.txt"
+// argv[2] = "movimientos.txt"
+// argv[3] = "100" //retardo
+// argv[4] = "salida.txt" opcional el último argumento
+
+
 int main(int argc, char *argv[]) {
     // Verificación de argumentos
     if (argc < 4) {
@@ -16,14 +30,15 @@ int main(int argc, char *argv[]) {
     string ficheroTablero = argv[1];
     string ficheroMovimientos = argv[2];
     int retardo = stoi(argv[3]);
+    // Si no se proporciona un fichero de salida en el argumento 5, se usa "resultado.txt" por defecto
     string ficheroSalida = (argc == 5) ? argv[4] : "resultado.txt";
 
-    // Estructuras principales
+    //struct principales
     tpTablero tablero;
     tpMovimientosValidos movimientosValidos;
     tpListaMovimientos solucion;
 
-    // Inicialización del tablero
+    // Inicialización del tablero según el fichero
     if (!inicializarTablero(ficheroTablero, tablero)) {
         cerr << "Error al inicializar el tablero desde el fichero: " << ficheroTablero << endl;
         return 1;
